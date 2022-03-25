c6ef98db183c461fce1fb745ca79a81453f8afa2
Timur Bochkarev <tmbochkarev@gmail.com>
2022-03-25 09:17:05 +0700

    Add files in repository

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+import Card from "./pages/components/Card";
+
+export default function App() {
+  return <Card greet="Hello again" />;
+}
---
+++ src/pages/styles/Card.ts
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const Button = styled.button`
+  border: solid 3px blue;
+  color: red;
+`;
