0ed13ca02331797a5e5cb095c0c3614332e1a274
Timur Bochkarev <tmbochkarev@gmail.com>
2022-03-26 19:45:28 +0700

    Add several buttons

--- src/pages/components/Card.tsx
+++ src/pages/components/Card.tsx
@@ -1,5 +1,7 @@
 // import { Button } from "../styles/Card";
 import Button from "react-bootstrap/Button";
+import { Row, Col } from "react-bootstrap";
+import Container from "react-bootstrap/Container";
 import { useState } from "react";
 
 export default function Card(props: any) {
@@ -10,6 +12,19 @@ export default function Card(props: any) {
       <Button variant="secondary" onClick={() => setCount(count + 1)}>
         {props.greet}
       </Button>
+      <Container fluid="md">
+        <Row className="mx-0">
+          <Button as={Col} variant="primary">
+            Button #1
+          </Button>
+          <Button as={Col} variant="secondary" className="mx-2">
+            Button #2
+          </Button>
+          <Button as={Col} variant="success">
+            Button #3
+          </Button>
+        </Row>
+      </Container>
       <p>{count}</p>
     </>
   );
