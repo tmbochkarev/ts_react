faee1612743cde007db1d2b36b2e07144583280c
Timur Bochkarev <tmbochkarev@gmail.com>
2022-03-26 19:09:12 +0700

    Alter Card component

--- src/pages/components/Card.tsx
+++ src/pages/components/Card.tsx
@@ -4,34 +4,10 @@ import { useState } from "react";
 export default function Card(props: any) {
   const [count, setCount] = useState(0);
 
-  let cities: any = [
-    { id: 1, name: "Berlin", population: 2000000 },
-    {
-      id: 2,
-      name: "Paris",
-      population: 4000000,
-    },
-    {
-      id: 3,
-      name: "Amsterdam",
-      population: 500000,
-    },
-    {
-      id: 4,
-      name: "Brussel",
-      population: 700000,
-    },
-  ];
-
-  const BIG_CITY_POPULATION: number = 2 * Math.pow(2, 6);
-
-  const mapCities = cities.map((city) => city);
-
   return (
     <>
       <Button onClick={() => setCount(count + 1)}>{props.greet}</Button>
       <p>{count}</p>
-      <p>{mapCities}</p>
     </>
   );
 }
