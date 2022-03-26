eb86cd89e47a48cfecbe031430f22b04db3bc607
Timur Bochkarev <tmbochkarev@gmail.com>
2022-03-26 19:31:12 +0700

    Add react-bootstrap

--- src/pages/components/Card.tsx
+++ src/pages/components/Card.tsx
@@ -1,4 +1,5 @@
-import { Button } from "../styles/Card";
+// import { Button } from "../styles/Card";
+import Button from "react-bootstrap/Button";
 import { useState } from "react";
 
 export default function Card(props: any) {
@@ -6,7 +7,9 @@ export default function Card(props: any) {
 
   return (
     <>
-      <Button onClick={() => setCount(count + 1)}>{props.greet}</Button>
+      <Button variant="secondary" onClick={() => setCount(count + 1)}>
+        {props.greet}
+      </Button>
       <p>{count}</p>
     </>
   );
