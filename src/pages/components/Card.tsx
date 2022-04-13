f19c8442675ed387476e05ed6fac0c8b484a744a
tmbochkarev <tmbochkarev@gmail.com>
2022-04-13 08:52:32 +0700

    Changed button styles

--- src/pages/components/Card.tsx
+++ src/pages/components/Card.tsx
@@ -1,31 +1,16 @@
-// import { Button } from "../styles/Card";
-import Button from "react-bootstrap/Button";
-import { Row, Col } from "react-bootstrap";
-import Container from "react-bootstrap/Container";
+import { Button } from '../styles/Card'
 import { useState } from "react";
+import '../styles/main.scss'
+
 
 export default function Card(props: any) {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <Button variant="secondary" onClick={() => setCount(count + 1)}>
-        {props.greet}
-      </Button>
-      <Container fluid="md">
-        <Row className="mx-0">
-          <Button as={Col} variant="primary">
-            Button #1
-          </Button>
-          <Button as={Col} variant="secondary" className="mx-2">
-            Button #2
-          </Button>
-          <Button as={Col} variant="success">
-            Button #3
-          </Button>
-        </Row>
-      </Container>
-      <p>{count}</p>
+    <p>hhhoh</p>
+    <h1>{count}</h1>
+    <Button onClick={() => setCount(count + 1)}>Put me</Button>
     </>
   );
 }
